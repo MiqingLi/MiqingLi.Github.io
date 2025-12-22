c963cfeae611c9b19e39eac1d30648809c673370
Zim-L <5424136+Zim-L@users.noreply.github.com>
2025-12-22 13:15:44 +0000

    Deploying to gh-pages from @ MiqingLi/MiqingLi.Github.io@6a6cc4914e3e619d82a91c819e231d0caa8248ba 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "MiqingLi/Miqing.github.io",
+    "data-repo": "MiqingLi/MiqingLi.Github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
